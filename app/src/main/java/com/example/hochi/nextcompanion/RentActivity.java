de5c508ac008669551be86bd6a0f6b815536f64d
ikarulus <github.ikarulus@zer.one>
2019-01-07 00:15:02 +0100

    RentActivity improve navigation

--- app/src/main/java/com/example/hochi/nextcompanion/RentActivity.java
+++ app/src/main/java/com/example/hochi/nextcompanion/RentActivity.java
@@ -1,5 +1,6 @@
 package com.example.hochi.nextcompanion;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class RentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rent);
+        ActionBar actionBar = getActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
 }
