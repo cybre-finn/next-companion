aed8758a85ed48630562759d8b18b3be4314c8cf
lgehr <lgehr@maibox.org>
2020-10-29 02:01:27 +0100

    Deletes unused code

--- app/src/main/java/com/example/hochi/nextcompanion/NextbikeResponseLogin.java
+++ app/src/main/java/com/example/hochi/nextcompanion/NextbikeResponseLogin.java
@@ -28,20 +28,6 @@ public class NextbikeResponseLogin {
         public int free_seconds;
         public String[] partner_ids;
 
-        // public NextbikeResponseLoginUser(String mobile, String loginkey, String[] rfid_uids, Boolean active,
-        //         String lang, String domain, String currency, int credits, int free_seconds, String[] partner_ids) {
-        //     this.mobile = mobile;
-        //     this.loginkey = loginkey;
-        //     this.rfid_uids = rfid_uids;
-        //     this.active = active;
-        //     this.lang = lang;
-        //     this.domain = domain;
-        //     this.currency = currency;
-        //     this.credits = credits;
-        //     this.free_seconds = free_seconds;
-        //     this.partner_ids = partner_ids;
-        // }
-
     }
 
     private String server_time;
@@ -53,31 +39,6 @@ public class NextbikeResponseLogin {
         this.server_time = server_time;
         this.user = user;
     }
-
-    // @JsonCreator
-    // public NextbikeResponseLogin(@JsonProperty("server_time") String server_time,
-    //                              @JsonProperty("mobile") String mobile,
-    //                              @JsonProperty("loginkey") String loginkey,
-    //                              @JsonProperty("rfid_uids") String[] rfid_uids,
-    //                              @JsonProperty("active") Boolean active,
-    //                              @JsonProperty("lang") String lang,
-    //                              @JsonProperty("domain") String domain,
-    //                              @JsonProperty("currency") String currency,
-    //                              @JsonProperty("credits") int credits,
-    //                              @JsonProperty("free_seconds") int free_seconds,
-    //                              @JsonProperty("partner_ids") String[] partner_ids ) {
-    //     this.server_time = server_time;
-    //     this.user.mobile = mobile;
-    //     this.user.loginkey = loginkey;
-    //     this.user.rfid_uids = rfid_uids;
-    //     this.user.active = active;
-    //     this.user.lang = lang;
-    //     this.user.domain = domain;
-    //     this.user.currency = currency;
-    //     this.user.credits = credits;
-    //     this.user.free_seconds = free_seconds;
-    //     this.user.partner_ids = partner_ids;
-    // }
 
     public NextbikeResponseLoginUser getUser() {
         return user;
