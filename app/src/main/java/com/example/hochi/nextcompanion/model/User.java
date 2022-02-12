3c7fa6819af91be5aa5add5f2e6484254a317954
loewenzahm <nico.graebling@posteo.net>
2022-02-12 13:59:44 +0100

    [Refactoring] Use Volley for HTTP-request and Jackson for Json-mapping in the LoginActivity
    This is done for the LoginActivity exemplary - if the review shows that this approach fits for all programmers we could use this for all requests in all other Activities as well
    
    
    parts of issue #19 (Volley + Jackson)
    
    parts of issue #13 (proper error handling using snackbar-messages)

---
+++ app/src/main/java/com/example/hochi/nextcompanion/model/User.java
@@ -0,0 +1,11 @@
+package com.example.hochi.nextcompanion.model;
+
+/**
+ * This class matches the api's user element
+ * Don't rename the attributes unless it is needed due to changes in the api
+ */
+public class User {
+    public String loginkey;
+    public String screen_name;
+    public String mobile;
+}
